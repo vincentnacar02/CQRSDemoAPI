e06db66b2707ccd7e7b58c9eeca0a4867d360537
Vincent Nacar <vnacar@racami.com>
2022-08-06 13:33:06 +0800

    added api gateway

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = new WebHostBuilder();
+            builder.UseKestrel();
+            builder.UseContentRoot(Directory.GetCurrentDirectory());
+            builder.ConfigureAppConfiguration((context, config) =>
+            {
+                config.SetBasePath(context.HostingEnvironment.ContentRootPath);
+                config.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                config.AddEnvironmentVariables();
+            });
+            builder.ConfigureServices((services) =>
+            {
+                services.AddOcelot();
+            });
+            builder.Configure(app =>
+            {
+                app.UseOcelot().Wait();
+            });
+            builder.Build().Run();
+        }
+    }
+}

e14f8c176c9fa57a199aa7bade81fbcbf811167c
Vincent Nacar <vnacar@racami.com>
2022-08-07 14:46:03 +0800

    demonstrate aggregate

---
+++ UserMicroservice/Domain/UserAggregate/User.cs
@@ -0,0 +1,32 @@
+﻿using Domain.UserAggregate;
+using System;
+using System.Collections.Generic;
+
+namespace Domain.UserAggregate
+{
+    public class User
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+
+        private List<UserAddress> _addresses;
+
+        public User()
+        {
+            this._addresses = new List<UserAddress>();
+        }
+
+        public void AddUserAddress(string address1, string city, string state, string zip, string country)
+        {
+            var newAddr = new UserAddress
+            {
+                Address1 = address1,
+                City = city,
+                State = state,
+                Zip = zip,
+                Country = country
+            };
+            _addresses.Add(newAddr);
+        }
+    }
+}
---
+++ UserMicroservice/Domain/UserAggregate/UserAddress.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.UserAggregate
+{
+    public class UserAddress
+    {
+        public String Address1 { get; set; }
+        public String City { get; set; }
+        public String State { get; set; }
+        public String Zip { get; set; }
+        public String Country { get; set; }
+    }
+}
